9aba49572946133175b4072bab2abb1a091dfcd2
Lenny <114218554+uvsq22202588@users.noreply.github.com>
2023-03-23 19:44:37 +0100

    Add files via upload
    Ajout d'un menu de départ et quelques changements sur la position de la fenêtre

--- main.py
+++ main.py
@@ -1,150 +1,242 @@
 # FICHIER DU GROUPE AMINA, ANDREA, LENNY, RASSIM, JEU DU PENDU
 # --------------------------------------
-# PARTIE LENNY
+
 from random import randint
-
-liste = ['aigle','buse','faucon','milan'] # avec les listes d'amina
-
-def mots(liste):
-    element = randint(0,len(liste)-1)
-    mot = liste[element]
-    return mot
-
-mot = mots(liste)
-
-def tiret():
-    global mot
-    affichage=[]
-    for i in range(len(mot)):
-        affichage.append('_')
-    return affichage
-
 import tkinter as tk
+from tkinter import messagebox
+import tkinter.font as font
 
-erreur = 0
+jeu = False
+
+fenetre_menu = tk.Tk()
+fenetre_menu.geometry("600x500+500+200")
+fenetre_menu.grid_columnconfigure(1, weight=1)
+fenetre_menu.grid_rowconfigure(1, weight=1)
+fenetre_menu.grid_rowconfigure(2, weight=1)
+fenetre_menu.grid_rowconfigure(3, weight=1)
+font_label = font.Font(size=20)
+font_bouton = font.Font(size=15)
+
+def jeu():
+    global jeu
+    jeu = True
+    fenetre_menu.destroy()
+
+def quitter():
+    fenetre_menu.destroy()
+
+Label_1 = tk.Label(fenetre_menu, text="Jeu du Pendu", fg = "black")
+Label_1['font'] = font_label
+
+bouton_jouer = tk.Button(fenetre_menu, text="Jouer", command=lambda : jeu())
+bouton_quitter = tk.Button(fenetre_menu, text="Quitter", command=lambda : quitter())
+bouton_jouer['font'] = font_bouton
+bouton_quitter['font'] = font_bouton
+
+Label_1.grid(row = 1, column = 1, columnspan = 2)
+bouton_jouer.grid(row = 2, column = 1)
+bouton_quitter.grid(row = 3, column = 1)
+
+            
+fenetre_menu.mainloop()
 
 
-def ecrit_les_coords(evt):
-    global erreur
-    pos_x, pos_y = evt.x, evt.y
-    erreur+=1
-    print(pos_x,pos_y)
+
+if jeu:
+    liste = ['AIGLE','BUSE','FAUCON','MILAN'] # avec les listes d'amina
+
+    def mots(liste):
+        element = randint(0,len(liste)-1)
+        mot = liste[element]
+        return mot
 
 
-
-fenetre = tk.Tk()
-fenetre.title("pendu")
-fenetre.geometry("900x700")
-
-mot_pointille = tiret()
-
-label_erreur = tk.Label(fenetre, text="Vous avez fait "+str(erreur)+" erreur(s)")
+    mot = mots(liste)
+    print(mot)
 
 
-def change_label_erreur():
-    global erreur
-    label_erreur.config(text="Vous avez fait "+str(erreur)+" erreur(s)")
-    fenetre.after(10, change_label_erreur)
-
-change_label_erreur()
-
-def lettre_dans_le_mot_ou_erreur(lettre):
-    global mot
-    global mot_pointille
-    global erreur
-    if lettre in mot:
+    def tiret():
+        global mot
+        affichage=[]
         for i in range(len(mot)):
-            if mot[i] == lettre:
-                mot_pointille[i] = lettre
-        return True
-    else: 
-        if erreur >= 12:
-            #tk.messagebox.M
-          erreur +=1
+            affichage.append('_')
+        return affichage
+
+
+    erreur = 0
+
+
+    def ecrit_les_coords(evt):
+        global erreur
+        pos_x, pos_y = evt.x, evt.y
+        erreur+=1
+        print(pos_x,pos_y)
+
+
+
+    fenetre = tk.Tk()
+    fenetre.title("pendu")
+    fenetre.geometry("600x500+500+200")
+
+    mot_pointille = tiret()
+
+    canvas_personnage = tk.Canvas(fenetre, width=500, height=400, bg="cyan")
+
+    label_erreur = tk.Label(fenetre, text="Vous avez fait "+str(erreur)+" erreur(s)")
+
+    reussite = 0
+
+    def change_label_erreur():
+        global erreur
+        label_erreur.config(text="Vous avez fait "+str(erreur)+" erreur(s)")
+        fenetre.after(10, change_label_erreur)
+
+
+
+    change_label_erreur()
+    lettre_deja_donne = []
+
+    liste_ligne=[[100,400,400,400],[150,400,150,150],[150,150,300,150],[200,150,150,200],[300,150,300,200],[325,250,275,200],[ 300,250,300,300],[300,275,325,275],[275,275,325,275],   [300,300,325,320],[300,300,275,320]]
+
+    def creer_lignes(liste_ligne, index, canevas):
+        x1, y1, x2, y2 = liste_ligne[index-1]
+        if index != 6:
+            canevas.create_line(x1, y1, x2, y2, fill="black")
+        else: canevas.create_oval(x1, y1, x2, y2, outline="black")
+
+    #for i in range(len(liste_ligne)):
+    #    creer_lignes(liste_ligne, i, canvas_personnage)
+
+
+    mot_pointille_shw = ""
+    for i in range(len(mot_pointille)):
+        mot_pointille_shw = ' '+mot_pointille[i]+mot_pointille_shw
+
+    label_pointille = tk.Label(fenetre, text=str(mot_pointille_shw))
+
+    def change_label_pointille(mot):
+        mot_pointille_shw = ""
+        for i in range(len(mot)):
+            mot_pointille_shw = mot_pointille_shw+' '+mot[i]
+        label_pointille.config(text=mot_pointille_shw)
+
+    def lettre_dans_le_mot_ou_erreur(lettre):
+        global mot
+        global mot_pointille
+        global erreur
+        global liste_ligne
+        global canvas_personnage
+        global reussite
+        global lettre_deja_donne
+
+        if lettre in mot:
+            for i in range(len(mot)):
+                if mot[i] == lettre:
+                    mot_pointille[i] = lettre
+            #print(mot_pointille)
+            if not (lettre in lettre_deja_donne):
+                change_label_pointille(mot_pointille)
+                reussite += 1
+                lettre_deja_donne.append(lettre)
+                #print(reussite)
+                #print(mot)
         
-boutonA=tk.Button(fenetre,text="A",command=lettre_dans_le_mot_ou_erreur("A"))
-boutonB=tk.Button(fenetre,text="B",command=lettre_dans_le_mot_ou_erreur("B"))
-boutonC=tk.Button(fenetre,text="C",command=lettre_dans_le_mot_ou_erreur("C"))
-boutonD=tk.Button(fenetre,text="D",command=lettre_dans_le_mot_ou_erreur("D"))
-boutonE=tk.Button(fenetre,text="E",command=lettre_dans_le_mot_ou_erreur("E"))
-boutonF=tk.Button(fenetre,text="F",command=lettre_dans_le_mot_ou_erreur("F"))
-boutonG=tk.Button(fenetre,text="G",command=lettre_dans_le_mot_ou_erreur("G"))
-boutonH=tk.Button(fenetre,text="H",command=lettre_dans_le_mot_ou_erreur("H"))
-boutonI=tk.Button(fenetre,text="I",command=lettre_dans_le_mot_ou_erreur("I"))
-boutonJ=tk.Button(fenetre,text="J",command=lettre_dans_le_mot_ou_erreur("J"))
-boutonK=tk.Button(fenetre,text="K",command=lettre_dans_le_mot_ou_erreur("K"))
-boutonL=tk.Button(fenetre,text="L",command=lettre_dans_le_mot_ou_erreur("L"))
-boutonM=tk.Button(fenetre,text="M",command=lettre_dans_le_mot_ou_erreur("M"))
-boutonN=tk.Button(fenetre,text="N",command=lettre_dans_le_mot_ou_erreur("N"))
-boutonO=tk.Button(fenetre,text="O",command=lettre_dans_le_mot_ou_erreur("O"))
-boutonP=tk.Button(fenetre,text="P",command=lettre_dans_le_mot_ou_erreur("P"))
-boutonQ=tk.Button(fenetre,text="Q",command=lettre_dans_le_mot_ou_erreur("Q"))
-boutonR=tk.Button(fenetre,text="R",command=lettre_dans_le_mot_ou_erreur("R"))
-boutonS=tk.Button(fenetre,text="S",command=lettre_dans_le_mot_ou_erreur("S"))
-boutonT=tk.Button(fenetre,text="T",command=lettre_dans_le_mot_ou_erreur("T"))
-boutonU=tk.Button(fenetre,text="U",command=lettre_dans_le_mot_ou_erreur("U"))
-boutonV=tk.Button(fenetre,text="V",command=lettre_dans_le_mot_ou_erreur("V"))
-boutonW=tk.Button(fenetre,text="W",command=lettre_dans_le_mot_ou_erreur("W"))
-boutonX=tk.Button(fenetre,text="X",command=lettre_dans_le_mot_ou_erreur("X"))
-boutonY=tk.Button(fenetre,text="Y",command=lettre_dans_le_mot_ou_erreur("Y"))
-boutonZ=tk.Button(fenetre,text="Z",command=lettre_dans_le_mot_ou_erreur("Z"))
 
-#print(mot_pointille)
-#lettre_dans_le_mot("m")
-#gprint(mot_pointille)
+        else: 
+            
+            print("Faux")
+            if erreur >= 11:
+                messagebox.showinfo(fenetre,message="Vous avez perdu !")
+                fenetre.destroy()
+            else: 
+                erreur +=1
+                creer_lignes(liste_ligne, erreur, canvas_personnage)
+        
+        if reussite == len(mot):
+            messagebox.showinfo(fenetre,message="Vous avez gagné !")
+            fenetre.destroy()
 
-canvas_personnage = tk.Canvas(fenetre, width=500, height=400, bg="cyan")
 
-#canvas_personnage.create_line(100,400,400,400,fill="black")
-#canvas_personnage.create_line(150,400,150,150,fill="black")
-#canvas_personnage.create_line(150,150,300,150,fill="black")    
-#canvas_personnage.create_line(200,150,150,200,fill="black")
-#canvas_personnage.create_line(300,150,300,200,fill="black")
-#canvas_personnage.create_oval(325,250,275,200,outline="black") # OVAL
-#canvas_personnage.create_line(300,250,300,300,fill="black")
-#canvas_personnage.create_line(300,275,325,275,fill="black")
-#canvas_personnage.create_line(275,275,325,275,fill="black")
-#canvas_personnage.create_line(300,300,325,320,fill="black")
-#canvas_personnage.create_line(300,300,275,320,fill="black")
 
-liste_ligne=[[100,400,400,400],[150,400,150,150],[150,150,300,150],[200,150,150,200],[300,150,300,200],[325,250,275,200],[ 300,250,300,300],[300,275,325,275],[275,275,325,275],   [300,300,325,320],[300,300,275,320]]
+            
+    boutonA=tk.Button(fenetre,text="A",command=lambda : lettre_dans_le_mot_ou_erreur("A"))
+    boutonB=tk.Button(fenetre,text="B",command=lambda : lettre_dans_le_mot_ou_erreur("B"))
+    boutonC=tk.Button(fenetre,text="C",command=lambda : lettre_dans_le_mot_ou_erreur("C"))
+    boutonD=tk.Button(fenetre,text="D",command=lambda : lettre_dans_le_mot_ou_erreur("D"))
+    boutonE=tk.Button(fenetre,text="E",command=lambda : lettre_dans_le_mot_ou_erreur("E"))
+    boutonF=tk.Button(fenetre,text="F",command=lambda : lettre_dans_le_mot_ou_erreur("F"))
+    boutonG=tk.Button(fenetre,text="G",command=lambda : lettre_dans_le_mot_ou_erreur("G"))
+    boutonH=tk.Button(fenetre,text="H",command=lambda : lettre_dans_le_mot_ou_erreur("H"))
+    boutonI=tk.Button(fenetre,text="I",command=lambda : lettre_dans_le_mot_ou_erreur("I"))
+    boutonJ=tk.Button(fenetre,text="J",command=lambda : lettre_dans_le_mot_ou_erreur("J"))
+    boutonK=tk.Button(fenetre,text="K",command=lambda : lettre_dans_le_mot_ou_erreur("K"))
+    boutonL=tk.Button(fenetre,text="L",command=lambda : lettre_dans_le_mot_ou_erreur("L"))
+    boutonM=tk.Button(fenetre,text="M",command=lambda : lettre_dans_le_mot_ou_erreur("M"))
+    boutonN=tk.Button(fenetre,text="N",command=lambda : lettre_dans_le_mot_ou_erreur("N"))
+    boutonO=tk.Button(fenetre,text="O",command=lambda : lettre_dans_le_mot_ou_erreur("O"))
+    boutonP=tk.Button(fenetre,text="P",command=lambda : lettre_dans_le_mot_ou_erreur("P"))
+    boutonQ=tk.Button(fenetre,text="Q",command=lambda : lettre_dans_le_mot_ou_erreur("Q"))
+    boutonR=tk.Button(fenetre,text="R",command=lambda : lettre_dans_le_mot_ou_erreur("R"))
+    boutonS=tk.Button(fenetre,text="S",command=lambda : lettre_dans_le_mot_ou_erreur("S"))
+    boutonT=tk.Button(fenetre,text="T",command=lambda : lettre_dans_le_mot_ou_erreur("T"))
+    boutonU=tk.Button(fenetre,text="U",command=lambda : lettre_dans_le_mot_ou_erreur("U"))
+    boutonV=tk.Button(fenetre,text="V",command=lambda : lettre_dans_le_mot_ou_erreur("V"))
+    boutonW=tk.Button(fenetre,text="W",command=lambda : lettre_dans_le_mot_ou_erreur("W"))
+    boutonX=tk.Button(fenetre,text="X",command=lambda : lettre_dans_le_mot_ou_erreur("X"))
+    boutonY=tk.Button(fenetre,text="Y",command=lambda : lettre_dans_le_mot_ou_erreur("Y"))
+    boutonZ=tk.Button(fenetre,text="Z",command=lambda : lettre_dans_le_mot_ou_erreur("Z"))
 
-canvas_personnage.bind("<Button-1>",ecrit_les_coords)
-def creer_lignes(liste_ligne, index, canevas):
-    x1, y1, x2, y2 = liste_ligne[index]
-    if index != 5:
-        canevas.create_line(x1, y1, x2, y2, fill="black")
-    else: canevas.create_oval(x1, y1, x2, y2, outline="black")
+    #print(mot_pointille)
+    #lettre_dans_le_mot("m")
+    #gprint(mot_pointille)
 
-boutonA.grid(column=1,row=0)
-boutonB.grid(column=1,row=1)
-boutonC.grid(column=1,row=2)
-boutonD.grid(column=1,row=3)
-boutonE.grid(column=1,row=4)
-boutonF.grid(column=1,row=5)
-boutonG.grid(column=1,row=6)
-boutonH.grid(column=1,row=7)
-boutonI.grid(column=1,row=8)
-boutonJ.grid(column=1,row=9)
-boutonK.grid(column=2,row=0)
-boutonL.grid(column=2,row=1)
-boutonM.grid(column=2,row=2)
-boutonN.grid(column=2,row=3)
-boutonO.grid(column=2,row=4)
-boutonP.grid(column=2,row=5)
-boutonQ.grid(column=2,row=6)
-boutonR.grid(column=2,row=7)
-boutonS.grid(column=2,row=8)
-boutonT.grid(column=2,row=9)
-boutonU.grid(column=3,row=0)
-boutonV.grid(column=3,row=1)
-boutonW.grid(column=3,row=2)
-boutonX.grid(column=3,row=3)
-boutonY.grid(column=3,row=4)
-boutonZ.grid(column=3,row=5) 
 
-canvas_personnage.grid(row=0, column=0, rowspan=10)
-label_erreur.grid(row=10,column=0)
-fenetre.mainloop()
-#
-# -----------------------------------------------------
+    #canvas_personnage.create_line(100,400,400,400,fill="black")
+    #canvas_personnage.create_line(150,400,150,150,fill="black")
+    #canvas_personnage.create_line(150,150,300,150,fill="black")    
+    #canvas_personnage.create_line(200,150,150,200,fill="black")
+    #canvas_personnage.create_line(300,150,300,200,fill="black")
+    #canvas_personnage.create_oval(325,250,275,200,outline="black") # OVAL
+    #canvas_personnage.create_line(300,250,300,300,fill="black")
+    #canvas_personnage.create_line(300,275,325,275,fill="black")
+    #canvas_personnage.create_line(275,275,325,275,fill="black")
+    #canvas_personnage.create_line(300,300,325,320,fill="black")
+    #canvas_personnage.create_line(300,300,275,320,fill="black")
+
+
+    canvas_personnage.bind("<Button-1>",ecrit_les_coords)
+    boutonA.grid(column=1,row=0)
+    boutonB.grid(column=1,row=1)
+    boutonC.grid(column=1,row=2)
+    boutonD.grid(column=1,row=3)
+    boutonE.grid(column=1,row=4)
+    boutonF.grid(column=1,row=5)
+    boutonG.grid(column=1,row=6)
+    boutonH.grid(column=1,row=7)
+    boutonI.grid(column=1,row=8)
+    boutonJ.grid(column=1,row=9)
+    boutonK.grid(column=2,row=0)
+    boutonL.grid(column=2,row=1)
+    boutonM.grid(column=2,row=2)
+    boutonN.grid(column=2,row=3)
+    boutonO.grid(column=2,row=4)
+    boutonP.grid(column=2,row=5)
+    boutonQ.grid(column=2,row=6)
+    boutonR.grid(column=2,row=7)
+    boutonS.grid(column=2,row=8)
+    boutonT.grid(column=2,row=9)
+    boutonU.grid(column=3,row=0)
+    boutonV.grid(column=3,row=1)
+    boutonW.grid(column=3,row=2)
+    boutonX.grid(column=3,row=3)
+    boutonY.grid(column=3,row=4)
+    boutonZ.grid(column=3,row=5) 
+
+    canvas_personnage.grid(row=0, column=0, rowspan=10)
+    label_erreur.grid(row=10,column=0)
+    label_pointille.grid(row = 11, column=0)
+
+
+    fenetre.mainloop()
+    #
+    # -----------------------------------------------------
