e41521971b5b5d0d453fa15117f8c703a2afb100
Lenny <114218554+uvsq22202588@users.noreply.github.com>
2023-03-23 14:27:22 +0100

    main.py
    2eme changement de main.py

--- main.py
+++ main.py
@@ -3,10 +3,15 @@
 # PARTIE LENNY
 import tkinter as tk
 
+erreur = 0
 
-def pixel_noir(evt):
+
+def ecrit_les_coords(evt):
+    global erreur
     pos_x, pos_y = evt.x, evt.y
+    erreur+=1
     print(pos_x,pos_y)
+
 
 
 fenetre = tk.Tk()
@@ -14,7 +19,15 @@ fenetre.title("pendu")
 fenetre.geometry("900x700")
 mot = "motteste"
 mot_pointille = ["_"]*len(mot)
-erreur = 0
+label_erreur = tk.Label(fenetre, text="Vous avez fait "+str(erreur)+" erreur(s)")
+
+
+def change_label_erreur():
+    global erreur
+    label_erreur.config(text="Vous avez fait "+str(erreur)+" erreur(s)")
+    fenetre.after(10, change_label_erreur)
+
+change_label_erreur()
 
 def lettre_dans_le_mot_ou_erreur(lettre):
     global mot
@@ -51,10 +64,12 @@ canvas_personnage = tk.Canvas(fenetre, width=500, height=400, bg="cyan")
 
 liste_ligne=[[100,400,400,400],[150,400,150,150],[150,150,300,150],[200,150,150,200],[300,150,300,200],[325,250,275,200],[ 300,250,300,300],[300,275,325,275],[275,275,325,275],   [300,300,325,320],[300,300,275,320]]
 
-canvas_personnage.bind("<Button-1>",pixel_noir)
+canvas_personnage.bind("<Button-1>",ecrit_les_coords)
+
 
 
 canvas_personnage.grid(row=0, column=0, rowspan=9, columnspan=3)
+label_erreur.grid(row=10,column=0)
 fenetre.mainloop()
 #
 # -----------------------------------------------------
